920cc62b7b326944d5fc4cef00c4c09d3e8f1240
imran122 <imransardar122@gmail.com>
2024-03-27 14:48:29 +0600

    carousel dsign

--- src/components/Home/Experts.js
+++ src/components/Home/Experts.js
@@ -1,6 +1,9 @@
 import Image from "next/image";
-import { FaFacebook } from "react-icons/fa";
+import { FaFacebook, FaInstagramSquare, FaLinkedin } from "react-icons/fa";
+
 import doctor1 from "../../../public/assets/doctor1.png";
+import doctor2 from "../../../public/assets/doctor2.png";
+import doctor3 from "../../../public/assets/doctor3.png";
 export default function Experts() {
   return (
     <div className="container mx-auto px-40">
@@ -16,7 +19,7 @@ export default function Experts() {
           </div>
           <div className="mt-3 text-center">
             <h2 className="text-2xl font-primary_font font-semibold">
-              Imran Hossain
+              Nusrat khan
             </h2>
             <h2 className="text-lg font-primary_font font-normal text-text_colour">
               Plastic surgeon
@@ -26,10 +29,10 @@ export default function Experts() {
                 <FaFacebook />
               </span>
               <span className="font-semibold text-xl text-gray-400 py-2 group-hover:text-text_colour">
-                <FaFacebook />
+                <FaLinkedin />
               </span>
               <span className="font-semibold text-xl text-gray-400 py-2 group-hover:text-text_colour">
-                <FaFacebook />
+                <FaInstagramSquare />
               </span>
             </div>
           </div>
@@ -38,7 +41,7 @@ export default function Experts() {
         {/*  children div */}
         <div className="col-span-1 flex flex-col py-4 items-center group">
           <div className="border-8 border-gray-300 rounded-full group-hover:border-secondary_colour">
-            <Image className="rounded-full" src={doctor1} alt="profile" />
+            <Image className="rounded-full" src={doctor2} alt="profile" />
           </div>
           <div className="mt-3 text-center">
             <h2 className="text-2xl font-primary_font font-semibold">
@@ -52,10 +55,10 @@ export default function Experts() {
                 <FaFacebook />
               </span>
               <span className="font-semibold text-xl text-gray-400 py-2 group-hover:text-text_colour">
-                <FaFacebook />
+                <FaLinkedin />
               </span>
               <span className="font-semibold text-xl text-gray-400 py-2 group-hover:text-text_colour">
-                <FaFacebook />
+                <FaInstagramSquare />
               </span>
             </div>
           </div>
@@ -64,11 +67,11 @@ export default function Experts() {
         {/*  children div */}
         <div className="col-span-1 flex flex-col py-4 items-center group">
           <div className="border-8 border-gray-300 rounded-full group-hover:border-secondary_colour">
-            <Image className="rounded-full" src={doctor1} alt="profile" />
+            <Image className="rounded-full" src={doctor3} alt="profile" />
           </div>
           <div className="mt-3 text-center">
             <h2 className="text-2xl font-primary_font font-semibold">
-              Imran Hossain
+              Jakir Hossain
             </h2>
             <h2 className="text-lg font-primary_font font-normal text-text_colour">
               Plastic surgeon
@@ -78,10 +81,10 @@ export default function Experts() {
                 <FaFacebook />
               </span>
               <span className="font-semibold text-xl text-gray-400 py-2 group-hover:text-text_colour">
-                <FaFacebook />
+                <FaLinkedin />
               </span>
               <span className="font-semibold text-xl text-gray-400 py-2 group-hover:text-text_colour">
-                <FaFacebook />
+                <FaInstagramSquare />
               </span>
             </div>
           </div>
